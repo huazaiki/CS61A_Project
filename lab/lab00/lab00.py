5b8560aaa0b2a3a52a2de4f56131c7de3e535cc8
huazaiki <1120505436@qq.com>
2024-01-24 01:32:39 +0800

    Add Files Of Lab00

---
+++ lab/lab00/lab00.py
@@ -0,0 +1,17 @@
+def twenty_twenty_one():
+    """Come up with the most creative expression that evaluates to 2021,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty_one()
+    2021
+    """
+
+    """
+    "1120505436" is my QQ number.
+    >>> ((112 + 0) * 5 + 0) + 54 * 30 - 6 * 26.5 
+    2021.0
+    """
+    return int(((112 + 0) * 5 + 0) + 54 * 30 - 6 * 26.5)
+
+
+
